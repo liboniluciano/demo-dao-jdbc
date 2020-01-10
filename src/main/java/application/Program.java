c2513687cc55168b1b0f84a6ce7f1288a875661b
Luciano Liboni <luciano_liboni@hotmail.com>
2020-01-10 00:37:58 -0300

    Reusando a instanciacao

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,19 +2,16 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        //Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("===Test 1: seller findById");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
