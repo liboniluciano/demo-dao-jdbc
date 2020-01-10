764e2bc3c988d903e9a9697dd8c9fc6eb5b0452a
Luciano Liboni <luciano_liboni@hotmail.com>
2020-01-10 01:11:46 -0300

    Corrigindo retorno dos find

--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -112,10 +112,9 @@ public class SellerDaoJDBC implements SellerDao {
                 }
                 Seller obj = instantiateSeller(rs, dep);
                 list.add(obj);
-                return list;
-            }
 
-            return null;
+            }
+            return list;
         }
         catch (SQLException e){
             throw new DbException(e.getMessage());
@@ -130,35 +129,36 @@ public class SellerDaoJDBC implements SellerDao {
     public List<Seller> findByDepartment(Department department) {
         PreparedStatement st = null;
         ResultSet rs = null;
-
         try {
             st = conn.prepareStatement(
-                    "SELECT seller.*,department.Name as DepName " +
-                    "FROM seller INNER JOIN department " +
-                    "ON seller.DepartmentId = department.Id " +
-                    "WHERE DepartmentId = ? " +
-                    "ORDER BY Name ");
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE DepartmentId = ? "
+                            + "ORDER BY Name");
 
             st.setInt(1, department.getId());
+
             rs = st.executeQuery();
 
             List<Seller> list = new ArrayList<>();
             Map<Integer, Department> map = new HashMap<>();
-            while(rs.next()){
+
+            while (rs.next()) {
+
                 Department dep = map.get(rs.getInt("DepartmentId"));
 
-                if(dep == null){
+                if (dep == null) {
                     dep = instantiateDepartment(rs);
                     map.put(rs.getInt("DepartmentId"), dep);
                 }
+
                 Seller obj = instantiateSeller(rs, dep);
                 list.add(obj);
-                return list;
             }
-
-            return null;
+            return list;
         }
-        catch (SQLException e){
+        catch (SQLException e) {
             throw new DbException(e.getMessage());
         }
         finally {
